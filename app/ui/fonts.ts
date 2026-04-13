30951750ffb79149b8849f56dbbdbaff1e15a2b5
Aser Alemu <asertesfaye@hotmail.com>
2026-04-13 18:04:21 +0300

    the first commit of the crash coourse from Next Js

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana } from 'next/font/google';
+import { Poppins } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
